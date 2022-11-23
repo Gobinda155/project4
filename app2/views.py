9bc9ddcbff21e53fc976907865ab9c03d4e38672
gobinda155 <beheragobinda155@gmail.com>
2022-11-23 22:40:50 +0530

    first commit

--- app2/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def gobinda(request):
-      return render(request,'gobinda.html')
--- project4/urls.py
+++ project4/urls.py
@@ -1,4 +1,4 @@
-"""project4 URL Configuration
+"""project5 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app2.views import *
-
+from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gobinda/',gobinda,name='gobinda'),
+    path('vikram/',vikram,name='vikram'),
 ]
+
+
+
